ae9a223654e151feced6969eb201d6b7f9087cc0
LucaArcudi <arcudilu@gmail.com>
2023-02-06 19:33:41 +0100

    fix

--- models/Accessory.php
+++ models/Accessory.php
@@ -5,15 +5,15 @@ class Accessory extends Product
 {
     public $color;
     public $material;
-    public $typeAccessory;
+    public $size;
 
-    public function __construct(Category $_category, $_brand, $_title, $_description, $_type, $_price, $_image, $_color, $_material, $type)
+    public function __construct(Category $_category, $_brand, $_title, $_description, $_type, $_price, $_image, $_color, $_material, $_size)
     {
         parent::__construct($_category, $_brand, $_title, $_description, $_type, $_price, $_image);
 
         $this->color = $_color;
         $this->material = $_material;
-        $this->typeAccessory = $_type;
+        $this->size = $_size;
     }
 
     public function getColor()
@@ -28,6 +28,6 @@ class Accessory extends Product
 
     public function getTypeAccessory()
     {
-        return $this->typeAccessory;
+        return $this->size;
     }
 }
